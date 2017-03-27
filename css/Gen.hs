09e2d73202edd53e73b59bfa9046a01a910c4011
Daniel Campoverde [alx741] <alx@sillybytes.net>
2017-03-27 13:07:33 -0500

    Fix posts preview responsiveness issue

--- css/Gen.hs
+++ css/Gen.hs
@@ -9,6 +9,8 @@ main :: IO ()
 main = do
     writeFile "default.css" $ unpack $ renderCss def
     writeFile "post.css" $ unpack $ renderCss post
+    writeFile "post-list.css" $ unpack $ renderCss postList
 
 def = $(cassiusFile "default.cassius") ()
 post = $(cassiusFile "post.cassius") ()
+postList = $(cassiusFile "post-list.cassius") ()
